598f5fbb60546bdb1e0304274be7a2ca11599dc5
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2021-02-13 12:34:09 -0500

    added to forms.py

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -8,18 +8,26 @@ class GroceryStoreForm(FlaskForm):
 
     # TODO: Add the following fields to the form class:
     # - title - StringField
+    title = StringField('title', [validators.Length(min=4, max=25)])
     # - address - StringField
+    address = StringField('Address', [validators.Length(min=4, max=25)])
     # - submit button
-    pass
+    submit = SubmitField('Submit')
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
 
     # TODO: Add the following fields to the form class:
     # - name - StringField
+    name = StringField('Username', [validators.Length(min=4, max=25)])
     # - price - FloatField
+    price = FloatField('Price')
     # - category - SelectField (specify the 'choices' param)
+    category = SelectField('Category', choices = ['Deli', 'Produce', 'Bakery', 'Pantry', 'Frozen', 'Other'])
     # - photo_url - StringField (use a URL validator)
+    photo_url = StringField('Photo URL', [validators.Length(min=4, max=25)])
     # - store - QuerySelectField (specify the `query_factory` param)
+    store = QuerySelectField('Store', query_factory = store)
     # - submit button
-    pass
+    submit = SubmitField('Submit')
+    
